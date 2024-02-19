961e101583f975b5d0153f6045e3d11eecd51066
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-02-19 11:39:38 +0530

    implement frontend

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="flex gap-5">
+      <div className="flex flex-col text-gray-600 font-[300] mb-5 ">
+        <img
+          className="w-12 h-12 rounded-full object-cover"
+          src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1288&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <span>Just Now</span>
+      </div>
+      <div className="max-w-[40%]">
+        <p className='bg-white py-2.5 px-5'>Hello! This is test message. how are you</p>
+        {/* <img
+          className="w-12 h-12 rounded-full object-cover"
+          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        /> */}
+      </div>
+    </div>
+  );
+};
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,7 +2,11 @@ import { Message } from './Message';
 
 export const Messages = () => {
   return (
-    <div className="bg-[#ddddf7] p-2.5 h-[calc(100%-110px)]">
+    <div className="bg-[#ddddf7] p-2.5 h-[calc(100%-110px)] overflow-scroll no-scrollbar">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
       <Message />
       <Message />
       <Message />
